1a007942561f2950181572e8197a203dd498efff
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-06-09 15:51:14 -0300

    Ejs guia 3

---
+++ Assets/Scripts/Ej1.cs
@@ -0,0 +1,60 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            HolaMundo();
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            EscribirCadena("Mi cadena es esta:");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Concatenador("mensaje" , 10);
+        }
+
+    }
+    //Funciones:
+
+    //Definir una función que escriba en consola “Hola Mundo!” al ser invocada.
+    //Invocar la función al presionar la barra espaciadora.
+
+    void HolaMundo()
+    {
+        Debug.Log("Hola mundo!");
+    }
+
+    ////Definir una función que escriba en consola la cadena pasada por parámetro.
+
+    void EscribirCadena(string cadena)
+    {
+        Debug.Log(cadena);
+    }
+
+    //Definir una función que concatene y muestre en consola un string y un int pasados por parámetros. 
+
+    void Concatenador(string text, int num)
+    {
+        string msg = text + num;
+        Debug.Log(msg);
+    }
+
+
+}
---
+++ Assets/Scripts/Ej2.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+    //Variables:
+    public GameObject miObj;
+    public GameObject otroObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PosicionarObj(miObj);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (PosicionYNoNegativa(otroObj) == true)
+            {
+                Debug.Log("'Y' no es negativa");
+            }
+            else
+            {
+                Debug.Log("'Y' es negativa");
+            }
+        }
+
+    }
+    //Funciones:
+
+    //Recibe como parámetro un objeto y lo posiciona en el origen del espacio.
+    void PosicionarObj(GameObject obj)
+    {
+        obj.transform.position = Vector3.zero;
+    }
+
+    //Devuelve true si el valor de Y de la posición de un GameObject...
+    //recibido como parámetro no es negativo, false si lo es.
+    bool PosicionYNoNegativa(GameObject obj)
+    {
+        bool yNoNegativa = false;
+
+        if(obj.transform.position.y >= 0)
+        {
+            yNoNegativa = true;
+        }
+
+        return yNoNegativa;
+    }
+}
